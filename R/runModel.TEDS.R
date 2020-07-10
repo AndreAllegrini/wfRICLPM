ca00c458fd092060ee09d233aea4f840253a1642
AndreAllegrini <38887793+AndreAllegrini@users.noreply.github.com>
2020-07-10 11:08:17 +0100

    Create runModel.TEDS.R

---
+++ R/runModel.TEDS.R
@@ -0,0 +1,23 @@
+library(lavaan)
+
+load("covobs_TEDS.gitHub.RData")
+
+wf_clpm.cov <- covObs #variance covariance matrix 
+wf_clpm.n <- list(DZ=5465,MZ=2979) #TEDS sample size 
+
+
+wf_RICLPM_git <- lavaan(wf_RICLPM, 
+                        sample.cov = wf_clpm.cov, 
+                        sample.nobs = wf_clpm.n,
+                        missing = 'ml',
+                        int.ov.free = F,
+                        int.lv.free = F,
+                        auto.fix.first = F,
+                        auto.fix.single = F,
+                        auto.cov.lv.x = F,
+                        auto.cov.y = F,
+                        auto.var = F)
+
+
+summary(wf_RICLPM_git, standardized = TRUE)
+
