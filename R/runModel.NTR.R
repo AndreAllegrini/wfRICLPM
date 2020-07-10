e9cdf58371f9bc30d0df788d0aaa9c6c79c291ca
AndreAllegrini <38887793+AndreAllegrini@users.noreply.github.com>
2020-07-10 11:25:03 +0100

    Create runModel.NTR.R

---
+++ R/runModel.NTR.R
@@ -0,0 +1,20 @@
+
+load("covobs_NTR.gitHub.RData")
+
+wf_clpm.cov <- covObs #variance covariance matrix 
+wf_clpm.n <- list(DZ=11072,MZ=6056) #TEDS sample size 
+
+wf_RICLPM_git <- lavaan(wf_RICLPM, 
+               sample.cov = wf_clpm.cov, 
+               sample.nobs = wf_clpm.n,
+               missing = 'ml',
+               int.ov.free = F,
+               int.lv.free = F,
+               auto.fix.first = F,
+               auto.fix.single = F,
+               auto.cov.lv.x = F,
+               auto.cov.y = F,
+               auto.var = F)
+
+
+summary(wf_RICLPM_git, standardized = TRUE)
